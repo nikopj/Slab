069f4bb806e88d07aa03b3a687473ed2c3fd7305
Nikola <nikopj@me.com>
2019-03-22 11:35:43 -0400

    for karol

--- misc_sims/ant_match_4L.m
+++ misc_sims/ant_match_4L.m
@@ -6,32 +6,28 @@ f = 915e6; w = 2*pi*f;
 THRESH = 1.5; % VSWR threshold
 
 L_arr = [0:0.1:10]*1e-9;
-C_arr = [10:0.1:25]*1e-12;
+C_arr = [1:0.1:25]*1e-12;
 Z_arr = [1j*w*L_arr, -1j./(w*C_arr)];
 l0 = length(Z_arr);
 
-ZS = 50; ZL = 7.8-1j*21;
+ZS = 50; ZL = 1.5+7.4*1j;
 
 lc = length(L_arr+1);
 
-pi_config = {'CLC'};
+pi_config = {'CL'};
 lc_bounds = containers.Map({'L','C'},{1:lc-1, lc:l0});
 
 figure
 m=0;
 for config = pi_config
-    l1=lc_bounds(config{1}(1)); l2=lc_bounds(config{1}(2)); l3=lc_bounds(config{1}(3));
-
-    Gamma = nan(length(l1),length(l2),length(l3));
-    Q = nan(length(l1),length(l2),length(l3));
+    l1=lc_bounds(config{1}(1)); l2=lc_bounds(config{1}(2));
+    
+    Gamma = nan(length(l1),length(l2));
     mymin = 2;
     for ii=1:length(l1)
         for jj=1:length(l2)
-            for kk=1:length(l3)
-                Zin = Zpi(ZS,Z_arr(l1(ii)),Z_arr(l2(jj)),Z_arr(l3(kk)),ZL);
-                Q(ii,jj,kk) = Qpi(ZS,Z_arr(l1(ii)),Z_arr(l3(kk)),ZL);
-                Gamma(ii,jj,kk) = (Zin-ZS)/(Zin+ZS);
-            end
+            Zin = ZS + par(Z_arr(ii), Z_arr(jj) + ZL);
+            Gamma(ii,jj) = (Zin-ZS)/(Zin+ZS);
         end
     end
 
@@ -39,12 +35,11 @@ for config = pi_config
 
     [x, HFx] = ZtoLC(Z_arr(l1),f);
     [y, HFy] = ZtoLC(Z_arr(l2),f);
-    [z, HFz] = ZtoLC(Z_arr(l3),f);
-    [X,Y,Z] = ndgrid(x,y,z);
+    [X,Y] = ndgrid(x,y);
     m=m+1;
     subplot(1,length(pi_config),m)
-    scatter3(X(I),Y(I),Z(I),Q(I).^2,abs(Gamma(I)))
-    xlabel("Z1 ("+HFx+")"); ylabel("Z2 ("+HFy+")"); zlabel("Z3 ("+HFz+")")
+    scatter(X(I),Y(I),1,abs(Gamma(I)))
+    xlabel("Z1 ("+HFx+")"); ylabel("Z2 ("+HFy+")");
     title(config)
     caxis([0,0.2]); colormap jet; colorbar; c=colorbar;
     c.Label.String = "|\Gamma|"; c.Label.Rotation = 0;
